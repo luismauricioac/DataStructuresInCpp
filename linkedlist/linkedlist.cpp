68a7855d1b4c3c05583c88aaeebe998903afc62f
Luis Alamo <luismalamoc@gmail.com>
2022-10-03 14:55:13 -0300

    feat: methods for linked list were added

--- linkedlist/linkedlist.cpp
+++ linkedlist/linkedlist.cpp
@@ -14,6 +14,49 @@ void printList(Node*n) {
     }
 }
 
+void insertAtTheFront(Node**head, int newValue){
+    // 1. prepare a new node
+    Node* newNode = new Node();
+    newNode->Value = newValue;
+    // 2. put it in front of current head
+    newNode->Next = *head;
+    // 3. move head of the list to point to the newNode
+    *head = newNode;
+}
+
+void insertAtTheEnd(Node**head, int newValue){
+    // 1. prepare a new node
+    Node* newNode = new Node();
+    newNode->Value = newValue;
+    newNode->Next = NULL;
+    // 2. if linked list is empty newNode will be the head node
+    if(*head == NULL){
+        *head = newNode;
+        return;
+    }
+    // 3. find the last node
+    Node* last = *head;
+    while(last->Next != NULL){
+        last = last->Next;
+    }
+    // 4. insert newNode after last node in the list
+    last->Next = newNode;
+}
+
+void insertAfter(Node*previous, int newValue){
+    // 1. chek if previous is null
+    if(previous == NULL){
+        cout << "Previous can not be null";
+        return;
+    }
+    // 2. prepare a newNode
+    Node* newNode = new Node();
+    newNode->Value = newValue;
+    // 3. insert newNode after previous
+    newNode->Next = previous->Next;
+    previous->Next = newNode;
+}
+
 void linkedList() {
     Node* head = new Node();
     Node* second = new Node();
@@ -26,6 +69,13 @@ void linkedList() {
     third->Value = 3;
     third->Next = NULL;
 
+    //insertAtTheFront(&head, -1);
+    //insertAtTheFront(&head, -2);
+
+    //insertAtTheEnd(&head, 4);
+    //insertAtTheEnd(&head, 5);
+
+    insertAfter(head, -1);
     printList(head);
 
     system("pause>0");
